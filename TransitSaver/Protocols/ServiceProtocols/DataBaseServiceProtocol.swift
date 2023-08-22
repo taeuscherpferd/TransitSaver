90ef9d0835da1cc942128d651f6c7bf05f3aac9c
taeuscherpferd <ibber@myokcorral.com>
2023-08-21 21:41:06 -0600

    Saving extra changes

---
+++ TransitSaver/Protocols/ServiceProtocols/DataBaseServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  DataBaseServiceProtocol.swift
+//  TransitSaver
+//
+//  Created by Kailean O'Keefe on 8/14/23.
+//
+
+import Foundation
+
+protocol DatabaseServiceProtocol {
+  
+}
---
+++ TransitSaver/Views/SettingsPage.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsPage.swift
+//  TransitSaver
+//
+//  Created by Kailean O'Keefe on 8/21/23.
+//
+
+import SwiftUI
+import Foundation
+
+struct SettingsPage : View {
+  var body: some View {
+    NavigationView {
+      
+    }
+  }
+}
